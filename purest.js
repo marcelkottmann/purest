c67358de3da4ebffcc60c1c598700baffb8b4ef3
Marcel Richter <marcel.richter@gmail.com>
2012-11-21 21:42:56 +0100

    multiple features added

--- purest.js
+++ purest.js
@@ -1,66 +1,175 @@
-function render(baseElement, directive) {
-    for(var prop in directive) {
-        console.log(prop);
-        if(directive.hasOwnProperty(prop)) {
-            console.log("-> hasOwnProperty");
-            var append = false;
-            var selector = prop;
-            if(selector.charAt(selector.length - 1) === '+') {
+var PUREST;
+(function (PUREST) {
+    function createSelector(prop) {
+        var domSelector = '';
+        var attributeSelector;
+        var textSearch;
+        var append = false;
+        var search = /([^{}\+@]*)@?([^{}\+]*)(\+|{.*})?/;
+        var result = search.exec(prop);
+        if(result.length > 0) {
+            domSelector = result[1];
+            attributeSelector = result[2];
+            if(result[3] === '+') {
                 append = true;
-                selector = selector.substr(0, selector.length - 1);
-            }
-            var criteria = selector.split('@');
-            var sel = criteria[0];
-            var attributeSel;
-            if(criteria.length > 1) {
-                attributeSel = criteria[1];
-            }
-            var element = sel === '.' ? baseElement : baseElement.querySelector(sel);
-            var rule = directive[prop];
-            console.log("found element: ");
-            console.log(element);
-            console.log("attribute selector: " + attributeSel);
-            if(typeof rule === 'undefined') {
-                console.log("UNDEFINED RULE");
-                alert('undefined');
             } else {
-                if(typeof rule === 'string') {
-                    console.log("rule is of type string2: " + rule);
-                    var value = directive[prop];
-                    if(typeof attributeSel === 'undefined') {
-                        if(append) {
-                            element.innerHTML += value;
-                        } else {
-                            element.innerHTML = value;
-                        }
-                    } else {
-                        if(append) {
-                            element.attributes[attributeSel].textContent += value;
-                        } else {
-                            element.attributes[attributeSel].textContent = value;
-                        }
-                    }
+                if(result[3] !== '') {
+                    textSearch = result[3];
+                }
+            }
+        } else {
+            alert("invalid selector: '" + prop + "'");
+        }
+        return {
+            domSelector: domSelector,
+            attributeSelector: attributeSelector,
+            textSearch: textSearch,
+            append: append
+        };
+    }
+    function findElement(baseElement, domSelector) {
+        return domSelector === '' ? baseElement : baseElement.querySelector(domSelector);
+    }
+    function removeOthersToo(baseElement, domSelector) {
+        do {
+            var element = findElement(baseElement, domSelector);
+            if(element != null) {
+                element.parentNode.removeChild(element);
+            }
+        }while(element != null)
+    }
+    function logSelector(selector) {
+        console.log("selector: ");
+        console.log(selector.domSelector);
+        console.log("attribute selector: ");
+        console.log(selector.attributeSelector);
+        console.log("text search: ");
+        console.log(selector.textSearch);
+        console.log("append: ");
+        console.log(selector.append);
+    }
+    function handleUndefinedRule(baseElement, element, selector) {
+        var ppar = element.parentNode;
+        ppar.removeChild(element);
+        removeOthersToo(baseElement, selector.domSelector);
+    }
+    function handleLoopRule(baseElement, element, selector, rule) {
+        var items = rule.items;
+        var eachFunc = rule.each;
+        var itemsEmpty = (typeof items === 'undefined') || (items.length == 0);
+        console.log("itemsEmpty: " + itemsEmpty);
+        if(!itemsEmpty && (typeof eachFunc !== 'undefined')) {
+            var ppar = element.parentNode;
+            ppar.removeChild(element);
+            removeOthersToo(baseElement, selector.domSelector);
+            for(var i = 0; i < items.length; i++) {
+                var templateNode = element.cloneNode(true);
+                renderApply(templateNode, eachFunc(items[i]));
+                ppar.appendChild(templateNode);
+            }
+        } else {
+            if(itemsEmpty && (typeof rule.alt === 'undefined')) {
+                var ppar = element.parentNode;
+                ppar.removeChild(element);
+                removeOthersToo(baseElement, selector.domSelector);
+            } else {
+                if(itemsEmpty && (typeof rule.alt !== 'undefined')) {
+                    renderApply(element, rule.alt);
+                }
+            }
+        }
+    }
+    function handleIncludeRule(baseElement, element, selector, rule) {
+        var includeNode = document.querySelector(rule.include);
+        var templateNode = includeNode.cloneNode(true);
+        for(var i = element.childNodes.length - 1; i >= 0; --i) {
+            element.removeChild(element.childNodes[i]);
+        }
+        element.appendChild(templateNode);
+        renderApply(templateNode, rule.directive);
+    }
+    function handleObjectRule(baseElement, element, selector, rule) {
+        console.log("rule is of type object");
+        if(rule.hasOwnProperty("items")) {
+            handleLoopRule(baseElement, element, selector, rule);
+        } else {
+            if(rule.hasOwnProperty("include")) {
+                handleIncludeRule(baseElement, element, selector, rule);
+            } else {
+                alert("unknown rule type: " + rule);
+            }
+        }
+    }
+    function handlePrimitiveTypeRule(baseElement, element, selector, rule) {
+        console.log("rule is of type primitive type: " + rule);
+        if(selector.attributeSelector === '') {
+            if(selector.append) {
+                element.innerHTML += rule;
+            } else {
+                if(typeof selector.textSearch !== 'undefined') {
+                    element.innerHTML = element.innerHTML.replace(selector.textSearch, rule);
                 } else {
-                    if(typeof rule === 'object') {
-                        console.log("rule is of type object");
-                        var items = rule.items;
-                        var eachFunc = rule.each;
-                        var ppar = element.parentNode;
-                        ppar.removeChild(element);
-                        for(var i = 0; i < items.length; i++) {
-                            var templateNode = element.cloneNode(true);
-                            render(templateNode, eachFunc(items[i]));
-                            ppar.appendChild(templateNode);
+                    element.innerHTML = rule;
+                }
+            }
+        } else {
+            if(selector.append) {
+                element.attributes[selector.attributeSelector].textContent += rule;
+            } else {
+                if(typeof selector.textSearch !== 'undefined') {
+                    element.attributes[selector.attributeSelector].textContent = element.attributes[selector.attributeSelector].textContent.replace(selector.textSearch, rule);
+                } else {
+                    element.attributes[selector.attributeSelector].textContent = rule;
+                }
+            }
+        }
+    }
+    function renderApply(baseElement, directive) {
+        if(typeof directive === 'string') {
+            directive = {
+                '': directive
+            };
+        }
+        for(var prop in directive) {
+            console.log('Processing property: ' + prop);
+            if(directive.hasOwnProperty(prop)) {
+                console.log("-> hasOwnProperty");
+                var selector = createSelector(prop);
+                logSelector(selector);
+                var element = findElement(baseElement, selector.domSelector);
+                var rule = directive[prop];
+                console.log("found element: ");
+                console.log(element);
+                if(element != null) {
+                    if(typeof rule === 'undefined') {
+                        handleUndefinedRule(baseElement, element, selector);
+                    } else {
+                        if(typeof rule === 'object') {
+                            handleObjectRule(baseElement, element, selector, rule);
+                        } else {
+                            handlePrimitiveTypeRule(baseElement, element, selector, rule);
                         }
                     }
                 }
             }
         }
     }
-}
-function renderPage(selector, directive) {
-    var templateNode = document.querySelector(selector);
-    var resultNode = templateNode.cloneNode(true);
-    render(resultNode, directive);
-    templateNode.parentNode.replaceChild(resultNode, templateNode);
-}
+    PUREST.renderApply = renderApply;
+    function render(selector, directive, target) {
+        var templateNode = document.querySelector(selector);
+        var resultNode = templateNode.cloneNode(true);
+        renderApply(resultNode, directive);
+        var parentNode = templateNode.parentNode;
+        if(typeof target === 'undefined') {
+            parentNode.replaceChild(resultNode, templateNode);
+        } else {
+            var targetNode = document.querySelector(target);
+            if(targetNode != null) {
+                parentNode.removeChild(templateNode);
+                targetNode.appendChild(resultNode);
+            }
+        }
+    }
+    PUREST.render = render;
+})(PUREST || (PUREST = {}));
+
--- src/main/ts/purest.ts
+++ src/main/ts/purest.ts
@@ -1,76 +1,201 @@
-function render(baseElement:HTMLElement, directive) {
-      for (var prop in directive) {
-        console.log(prop);
-        if (directive.hasOwnProperty(prop)) {
-            console.log("-> hasOwnProperty");
-            
-            var append = false;
-            
-            var selector = prop;
-            if (selector.charAt(selector.length - 1) === '+') {
+module PUREST {
+
+    function createSelector(prop: string) {
+        var domSelector = '';
+        var attributeSelector;
+        var textSearch;
+        var append = false;
+
+        var search = /([^{}\+@]*)@?([^{}\+]*)(\+|{.*})?/;
+        var result = search.exec(prop);
+
+        if (result.length > 0) {
+            domSelector = result[1];
+            attributeSelector = result[2];
+            if (result[3] === '+') {
                 append = true;
-                selector = selector.substr(0, selector.length - 1);
+            } else if (result[3] !== '') {
+                textSearch = result[3];
             }
+        } else {
+            alert("invalid selector: '" + prop + "'");
+        }
+        return {
+            domSelector: domSelector,
+            attributeSelector: attributeSelector,
+            textSearch: textSearch,
+            append: append
+        };
+    }
 
-            var criteria = selector.split('@');
-            var sel = criteria[0];
-            var attributeSel;
-            
-            if (criteria.length > 1) {
-                attributeSel = criteria[1];
+
+    function findElement(baseElement: HTMLElement, domSelector: string) {
+        return domSelector === '' ? baseElement : <HTMLElement>baseElement.querySelector(domSelector);
+    }
+
+    function removeOthersToo(baseElement: HTMLElement, domSelector) {
+        do {
+            var element = findElement(baseElement, domSelector);
+            if (element != null) {
+                element.parentNode.removeChild(element);
             }
+        } while (element != null);
+    }
 
-            var element = sel==='.'?baseElement:<HTMLElement>baseElement.querySelector(sel);
-            var rule = directive[prop];
+    function logSelector(selector) {
 
-            console.log("found element: ");
-            console.log(element);
+        console.log("selector: ");
+        console.log(selector.domSelector);
 
-            console.log("attribute selector: "+attributeSel);
 
-            if (typeof rule === 'undefined') {
-                console.log("UNDEFINED RULE");
-                alert('undefined');
-            }else if (typeof rule === 'string') {
-                console.log("rule is of type string2: "+rule);
-                var value = directive[prop];
-                if (typeof attributeSel === 'undefined') {
-                    if (append) {
-                        element.innerHTML += value;
+        console.log("attribute selector: ");
+        console.log(selector.attributeSelector);
+
+        console.log("text search: ");
+        console.log(selector.textSearch);
+
+        console.log("append: ");
+        console.log(selector.append);
+    }
+
+    function handleUndefinedRule(baseElement: HTMLElement, element: HTMLElement, selector) {
+        var ppar = element.parentNode;
+        ppar.removeChild(element);
+        removeOthersToo(baseElement, selector.domSelector);
+    }
+
+    function handleLoopRule(baseElement: HTMLElement, element: HTMLElement, selector, rule) {
+        var items = rule.items;
+        var eachFunc = rule.each;
+        var itemsEmpty = (typeof items === 'undefined') || (items.length == 0);
+
+        console.log("itemsEmpty: " + itemsEmpty);
+
+        if (!itemsEmpty && (typeof eachFunc !== 'undefined')) {
+            //remove template element
+            var ppar = element.parentNode;
+            ppar.removeChild(element);
+            removeOthersToo(baseElement, selector.domSelector);
+
+            for (var i = 0; i < items.length; i++) {
+                //clone template
+                var templateNode = <HTMLElement>element.cloneNode(true);
+                renderApply(templateNode, eachFunc(items[i]));
+                ppar.appendChild(templateNode);
+            }
+        } else if (itemsEmpty && (typeof rule.alt === 'undefined')) {
+            //remove template element
+            var ppar = element.parentNode;
+            ppar.removeChild(element);
+            removeOthersToo(baseElement, selector.domSelector);
+        } else if (itemsEmpty && (typeof rule.alt !== 'undefined')) {
+            renderApply(element, rule.alt);
+        }
+    }
+
+    function handleIncludeRule(baseElement: HTMLElement, element: HTMLElement, selector, rule) {
+        var includeNode = <HTMLElement>document.querySelector(rule.include);
+        var templateNode = <HTMLElement>includeNode.cloneNode(true);
+
+        //remove all child nodes
+        for (var i = element.childNodes.length - 1; i >= 0; --i) {
+            element.removeChild(element.childNodes[i]);
+        }
+
+        element.appendChild(templateNode);
+        renderApply(templateNode, rule.directive);
+    }
+
+    function handleObjectRule(baseElement: HTMLElement, element: HTMLElement, selector, rule) {
+        console.log("rule is of type object");
+
+        //handle for loops
+        if (rule.hasOwnProperty("items")) {
+            handleLoopRule(baseElement, element, selector, rule);
+        }
+            //handle for includes
+        else if (rule.hasOwnProperty("include")) {
+            handleIncludeRule(baseElement, element, selector, rule);
+        }
+            //handle unknown rule types
+        else {
+            alert("unknown rule type: " + rule);
+        }
+    }
+
+    function handlePrimitiveTypeRule(baseElement: HTMLElement, element: HTMLElement, selector, rule) {
+        console.log("rule is of type primitive type: " + rule);
+
+        if (selector.attributeSelector === '') {
+            if (selector.append) {
+                element.innerHTML += rule;
+            } else if (typeof selector.textSearch !== 'undefined') {
+                element.innerHTML = element.innerHTML.replace(selector.textSearch, rule);
+            } else {
+                element.innerHTML = rule;
+            }
+        } else {
+            if (selector.append) {
+                element.attributes[selector.attributeSelector].textContent += rule;
+            } else if (typeof selector.textSearch !== 'undefined') {
+                element.attributes[selector.attributeSelector].textContent =
+                    element.attributes[selector.attributeSelector].textContent.replace(selector.textSearch, rule);
+            } else {
+                element.attributes[selector.attributeSelector].textContent = rule;
+            }
+        }
+    }
+
+    export function renderApply(baseElement: HTMLElement, directive) {
+
+        if (typeof directive === 'string') {
+            //create simple directive
+            directive = { '': directive };
+        }
+
+        for (var prop in directive) {
+            console.log('Processing property: ' + prop);
+
+            if (directive.hasOwnProperty(prop)) {
+                console.log("-> hasOwnProperty");
+
+                var selector = createSelector(prop);
+                logSelector(selector);
+
+                var element = findElement(baseElement, selector.domSelector);
+                var rule = directive[prop];
+
+                console.log("found element: ");
+                console.log(element);
+
+                if (element != null) {
+                    if (typeof rule === 'undefined') {
+                        handleUndefinedRule(baseElement, element, selector);
+                    } else if (typeof rule === 'object') {
+                        handleObjectRule(baseElement, element, selector, rule);
                     } else {
-                        element.innerHTML = value;
+                        handlePrimitiveTypeRule(baseElement, element, selector, rule);
                     }
-                } else {
-                    if (append) {
-                        element.attributes[attributeSel].textContent += value;
-                    } else {
-                        element.attributes[attributeSel].textContent = value;
-                    }
-                }
-            } else if (typeof rule === 'object') {
-                console.log("rule is of type object");
-                var items = <Array>rule.items;
-                var eachFunc = rule.each;
-
-                //remove template element
-                var ppar= element.parentNode;
-                ppar.removeChild(element);
-                
-                for (var i = 0; i < items.length;i++)
-                {
-                    //clone template
-                    var templateNode = <HTMLElement>element.cloneNode(true);
-                    render(templateNode, eachFunc(items[i])); 
-                    ppar.appendChild(templateNode);
                 }
             }
         }
     }
-}
 
-function renderPage(selector:string, directive) {
-    var templateNode = document.querySelector(selector);
-    var resultNode = <HTMLElement>templateNode.cloneNode(true);
-    render(resultNode, directive);
-    templateNode.parentNode.replaceChild(resultNode, templateNode);
+    export function render(selector: string, directive, target?: string) {
+        var templateNode = <HTMLElement>document.querySelector(selector);
+        var resultNode = <HTMLElement>templateNode.cloneNode(true);
+
+        renderApply(resultNode, directive);
+        var parentNode = templateNode.parentNode;
+
+        if (typeof target === 'undefined') {
+            parentNode.replaceChild(resultNode, templateNode);
+        } else {
+            var targetNode = <HTMLElement>document.querySelector(target);
+            if (targetNode != null) {
+                parentNode.removeChild(templateNode);
+                targetNode.appendChild(resultNode);
+            }
+        }
+    }
 }
